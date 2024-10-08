222f4c978e90e1869dadb0acadc8e69635cffcb3
keita1899 <saboten18k@gmail.com>
2024-10-09 07:44:44 +0900

    キャンセルボタン追加

--- src/components/EditTodoNameForm.tsx
+++ src/components/EditTodoNameForm.tsx
@@ -27,6 +27,13 @@ export const EditTodoNameForm = ({ id, name }: EditTodoNameFormProps) => {
         onChange={(e) => setEditTodoName(e.target.value)}
         className='border border-gray-300 rounded px-2 py-1'
       />
+      <button
+        type='button'
+        onClick={() => dispatch(toggleEditTodo(id))}
+        className='bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75'
+      >
+        キャンセル
+      </button>
       <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'>
         保存
       </button>
